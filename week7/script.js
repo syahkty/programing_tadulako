b617600db46a8a708473d8f6da64ae9a1bb90258
Syahkty <andisyahktu46@gmail.com>
2024-03-17 17:39:05 +0800

    komit tambahan

---
+++ week7/script.js
@@ -0,0 +1,63 @@
+// function luasPersegi() {
+//   sisi = prompt("masukkan sisi nya");
+//   console.log("luas persegi", sisi * sisi);
+// }
+
+// luasPersegi();
+
+// function luasPersegiPanjang(panjang, lebar) {
+//   console.log("luas persegi panjang", panjang * lebar);
+// }
+
+// let luas = function (sisi) {
+//   console.log(sisi);
+// };
+
+// const tes = (a, b) => {
+//   c = 0.5 * a * b;
+//   console.log(c);
+// };
+
+// const n = (nama) => "nama saya " + nama;
+
+// console.log(n("sakty"));
+// console.log(n);
+
+// const cekWarna = (color) => {
+//     if(color == "blue") {
+//         return "warna biru"
+//     } else {
+//         return "bukan merah"
+//     }
+// }
+
+// for(let i = 0; i < panjangNama; i++){
+//     console.log(nama[i])
+// }
+
+// let i = 0
+// while(i < panjangNama){
+//     console.log(nama[i])
+// }
+
+// let nama = ["farhan", "saleh", "juan", "semesta", "anjay", "mabar"];
+// const tampilkanSeluruhNama = (nama) => {
+//   let panjangNama = nama.length;
+//   for (let i = 0; i < panjangNama; i++) {
+//     console.log(nama[i]);
+//   }
+// };
+
+// let nama = "andi syahkty alifah assalam";
+
+// tes = (nama.split(" "));
+
+// for(let i = 0; i < tes.length;i++) {
+//     console.log(tes[i])
+// }
+
+// let umur = 10
+
+angka = (Math.round(Math.random() * 100));
+
+
